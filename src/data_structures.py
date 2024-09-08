223e74b89471ee61cceb9076b6636eaa74678276
Nicolas Bolle <nicolasbolle@gmail.com>
2024-09-08 15:19:46 -0400

    refactor

---
+++ src/data_structures.py
@@ -0,0 +1,391 @@
+"""Data structures for graph searches
+FIXME need to give ways to specify how verbose of a description should be printed, ex. for printing a Path object
+"""
+
+from abc import ABC, abstractmethod
+from typing import Self
+import time
+import numpy as np
+
+
+class Edge(ABC):
+    """Abstract graph edge
+    Lightweight, with usage defined in Node
+    """
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class Node(ABC):
+    """Abstract graph node"""
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __hash__(self):
+        """Hash for determining equality
+        Allows avoiding node revists in the solver
+        """
+
+    def __eq__(self, node):
+        return hash(self) == hash(node)
+
+    def __add__(self, edge: Edge):
+        return self.move(edge)
+
+    def __sub__(self, edge: Edge):
+        return self.unmove(edge)
+
+    @property
+    @abstractmethod
+    def solved(self) -> bool:
+        """Whether the node is in a "solved" state"""
+
+    @property
+    @abstractmethod
+    def edges(self) -> list[Edge]:
+        """Edges we can move along from the node"""
+
+    @abstractmethod
+    def move(self, edge: Edge) -> Self:
+        """Movement along an edge, returning the new node"""
+
+    @abstractmethod
+    def unmove(self, edge: Edge) -> Self:
+        """Undo movement along an edge, returning the new node"""
+
+    @abstractmethod
+    def _check_move(self, edge: Edge) -> bool:
+        """Check validity of moving along an edge"""
+
+    @abstractmethod
+    def _check_unmove(self, edge: Edge) -> bool:
+        """Check validity of unmoving along an edge"""
+
+
+class Path:
+    """Lightweight object recording a path in the graph of nodes"""
+
+    start_node: Node
+    edges: list[Edge]
+    end_node: Node
+
+    def __init__(self, start_node, edges, end_node):
+        self.start_node = start_node
+        self.edges = edges
+        self.end_node = end_node
+
+    def __repr__(self):
+        return (
+            f"Path({repr(self.start_node)}, {repr(self.edges)}, {repr(self.end_node)})"
+        )
+
+    def __str__(self):
+        out_list = []
+        if len(self.edges):
+            out_list.append(str(self.start_node))
+            out_list.append("")
+            out_list.append("to")
+            out_list.append("")
+            out_list.append(str(self.end_node))
+            out_list.append("")
+            out_list.append("via")
+            out_list.append("")
+            for edge in self.edges:
+                out_list.append(str(edge))
+        else:
+            out_list.append(str(self.start_node))
+            out_list.append("")
+            out_list.append("with no moves")
+        return "\n".join(out_list)
+
+    def get_str_end_node(self):
+        """String for just the end node"""
+        return str(self.end_node)
+
+    def __len__(self):
+        return len(self.edges)
+
+
+class Queue(ABC):
+    """Abstract queue for paths"""
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        return "\n\n----------\n\n".join([str(path) for path in self])
+
+    def get_str_end_nodes(self):
+        """String for just the end nodes"""
+        return "\n\n----------\n\n".join([path.get_str_end_node() for path in self])
+
+    @abstractmethod
+    def __len__(self):
+        """Length of the queue"""
+
+    @abstractmethod
+    def __iter__(self):
+        """Queue iterable, without altering the queue"""
+
+    @property
+    def empty(self) -> bool:
+        """True if the queue is empty"""
+        return len(self) == 0
+
+    @abstractmethod
+    def push(self, path: Path):
+        """Push a path onto the queue"""
+
+    @abstractmethod
+    def pop(self) -> Path:
+        """Pop the top path off the queue"""
+
+
+class Solver(ABC):  # pylint: disable=too-many-instance-attributes
+    """Abstract graph search solver
+    Subclass to specify the queue object
+    Does not consider path length: only attempts to find a solved node
+    """
+
+    # starting node of the problem instance
+    start_node: Node
+
+    # queue of path objects
+    queue: Queue
+
+    # hashes of nodes covered
+    # updated when adding to the queue
+    nodes_visited_hashes: set[int]
+
+    # solver progress tracking on the the overall optimization
+    # times recorded as floats following time.time()
+    solving_started: bool = False
+    solving_finished: bool = False
+    solved: bool = False
+    solution: Path = None
+    t_start_overall: float = None
+    t_end_overall: float = None
+    n_seconds_overall: int = 0
+    n_iters_overall: int = 0
+
+    # solver progress tracking on the current "sprint" of solving
+    t_start_sprint: float = None
+    t_end_sprint: float = None
+    n_seconds_sprint: int = None
+    n_iters_sprint: int = None
+    status_code = 300
+
+    @staticmethod
+    @abstractmethod
+    def get_queue_type():
+        """Subclass to specify the type of queue to use"""
+        return Queue
+
+    def __init__(self, start_node: Node):
+        # initialize fields
+        self.start_node = start_node
+        self.queue = self.get_queue_type()()  # pylint: disable=abstract-class-instantiated
+        self.nodes_visited_hashes = set()
+
+        # define the starting point for the search by "sniffing" the null path
+        path = Path(self.start_node, [], self.start_node)
+        self.sniff(path)
+
+    def __repr__(self):
+        return f"{type(self).__name__}({repr(self.start_node)})"
+
+    def __str__(self):
+        # FIXME add more info?
+        return f"{type(self).__name__} solver"
+
+    def sniff(self, path: Path):
+        """ "Sniff" a path to see if it's worth exploring"""
+        # check if we already have a solution
+        if self.solved:
+            return
+
+        # check if this path is solved
+        if path.end_node.solved:
+            self.solved = True
+            self.solution = path
+            return
+
+        # check if the end node of this path has been explored
+        _hash = hash(path.end_node)
+        if _hash in self.nodes_visited_hashes:
+            return
+
+        # record a visit to the end node of this path and add it to the queue
+        self.nodes_visited_hashes.add(path)
+        self.queue.push(path)
+
+    def explore(self, path: Path):
+        """ "Explore" a path: sniff out the results of adding on the next edges"""
+        if self.solved:
+            return
+        for edge in path.end_node.edges:
+            start_node = path.start_node
+            edges = path.edges + [edge]
+            end_node = path.end_node + edge
+            path_new = Path(start_node, edges, end_node)
+            self.sniff(path_new)
+            if self.solved:
+                return
+
+    def step(self):
+        """One step in the graph search process: explore the top of the queue"""
+        assert not self.queue.empty, "Queue is depleted"
+        path = self.queue.pop()
+        self.explore(path)
+
+    def steps(self, steps: int):
+        """Do multiple steps"""
+        for _ in range(steps):
+            self.step()
+
+    def solve(self, max_seconds: int | None = 10, max_iters: int | None = None) -> int:
+        """Run steps of the solving process and return status codes
+
+        Status codes: divided into classes
+            200-299: solution found
+                200: solution found
+            300-399: iteration terminated with queue remaining
+                A solution may exist, but wasn't found with the current iteration constraints
+                300: no iterations have been done yet
+                301: maximum time reached
+                302: maximum iterations reached
+            400-499: queue depleted without finding a solution
+                400: queue depleted without finding a solution
+
+        Parameters
+        ----------
+            max_seconds: int, optional
+                Maximum number of seconds to iterate (default is 10)
+                If unspecified, runs without bound on iteration time
+            max_iters: int, optional
+                Maximum number of iterations to run (default is None)
+                If unspecified, runs without bound on number of iterations
+
+        Returns
+        -------
+            status_code: int
+                Status code for the result of the iteration, see above for details
+        """
+        if max_seconds is None:
+            max_seconds = np.inf
+        if max_iters is None:
+            max_iters = np.inf
+
+        # set up progress tracking fields
+        t_start = time.time()
+
+        self.t_start_sprint = t_start
+        self.t_end_sprint = None
+        self.n_seconds_sprint = 0
+        self.n_iters_sprint = 0
+        self.status_code = None
+
+        if not self.solving_started:
+            self.solving_started = True
+            self.solving_finished = False
+            self.t_start_overall = t_start
+            self.t_end_overall = None
+            self.n_seconds_overall = 0
+            self.n_iters_overall = 0
+
+        # separate fields for tracking iteration termination
+        t_max = t_start + max_seconds
+        n_iters = 0
+
+        # loop until iteration is terminated
+        while True:
+            if self.solved:
+                status_code = 200
+                break
+
+            if self.queue.empty:
+                status_code = 400
+                break
+
+            if time.time() > t_max:
+                status_code = 301
+                break
+
+            if n_iters > max_iters:
+                status_code = 302
+                break
+
+            self.step()
+            n_iters += 1
+
+        # update progress tracking fields
+        t_end = time.time()
+
+        self.t_end_sprint = t_end
+        self.n_seconds_sprint = self.t_end_sprint - self.t_start_sprint
+        self.n_iters_sprint = n_iters
+        self.status_code = status_code
+
+        if (200 <= self.status_code < 300) or (400 <= self.status_code < 500):
+            self.solving_finished = True
+        self.t_end_overall = t_end
+        self.n_seconds_overall += self.n_seconds_sprint
+        self.n_iters_overall += self.n_iters_sprint
+
+        return self.status_code
+
+
+class BFSQueue(Queue):
+    """Breadth-first-search queue"""
+
+    queue: list[Path]
+
+    def __init__(self):
+        self.queue = []
+
+    def __repr__(self):
+        return "BFSQueue()"
+
+    def __len__(self):
+        """Length of the queue"""
+        return len(self.queue)
+
+    def __iter__(self):
+        """Queue iterable, without altering the queue"""
+        return iter(self.queue)
+
+    def push(self, path: Path):
+        """Push a path onto the queue"""
+        self.queue.append(path)
+
+    def pop(self) -> Path:
+        """Pop the top path off the queue"""
+        return self.queue.pop(0)
+
+
+class PriorityQueue(Queue):
+    """Priority queue"""
+
+    # FIXME stub
+
+
+class BFSSolver(Solver):
+    """Solver using a BFS queue"""
+
+    @staticmethod
+    def get_queue_type():
+        return BFSQueue
---
+++ src/sokoban_solver.py
@@ -0,0 +1,276 @@
+"""Classes for a sokoban solver"""
+
+from typing import Self
+import numpy as np
+from src.data_structures import Edge, Node, BFSSolver
+
+
+PRINTING_SYMBOLS = {
+    "wall": "█",
+    "air": " ",
+    "box": "□",
+    "player": "○",
+    "goal": "·",
+    "goal and box": "■",
+    "goal and player": "●",
+}
+
+WALL_THICKNESS = 2
+
+
+type Point = tuple[int, int]
+type Vector = tuple[int, int]
+
+
+class SokobanEdge(Edge):
+    """A box-pushing move in a sokoban puzzle"""
+
+    vector: Vector  # player movement
+    point1: Point  # original player location
+    point2: Point  # destination player location / original box location
+    point3: Point  # destination box location
+
+    def __init__(self, vector: Vector, point1: Point, point2: Point, point3: Point):
+        self.vector = vector
+        self.point1 = point1
+        self.point2 = point2
+        self.point3 = point3
+        assert self.valid
+
+    def __repr__(self):
+        return f"SokobanEdge({repr(self.vector)}, {repr(self.point1)}, {repr(self.point2)}, {repr(self.point3)})"
+
+    def __str__(self):
+        return f"Move {self.vector} from {self.point1} to {self.point2}"
+
+    @property
+    def valid(self):
+        """Checking the edge we represent is valid"""
+        dx, dy = self.vector
+        x1, y1 = self.point1
+        x2, y2 = self.point2
+        x3, y3 = self.point3
+
+        return (
+            (x1 + dx == x2) and (x2 + dx == x3) and (y1 + dy == y2) and (y2 + dy == y3)
+        )
+
+
+class SokobanNode(Node):  # pylint: disable= too-many-instance-attributes
+    """A sokoban board: air spaces, boxes, goals, the player
+    "air" really means "non wall", so these include locations of blocks, goals, and the player
+    Two boards are considered equal if one can reach the other via non-box-pushing moves
+    """
+
+    airs: set[Point]
+    boxes: set[Point]
+    goals: set[Point]
+    player: set[Point]
+
+    _player_component: set[Point]
+    _edges: list[SokobanEdge]
+
+    _hash: int
+    _str: str
+
+    def __init__(
+        self, airs: set[Point], boxes: set[Point], goals: set[Point], player: Point
+    ):
+        assert player in airs
+        assert len(boxes - airs) == 0
+        assert len(goals - airs) == 0
+        assert len(boxes) == len(goals)
+
+        self.airs = airs
+        self.boxes = boxes
+        self.goals = goals
+        self.player = player
+
+        self._player_component = None
+        self._edges = None
+
+        self._hash = None
+        self._str = None
+
+    def __repr__(self):
+        return f"SokobanNode({self.airs}, {self.boxes}, {self.goals}, {self.player})"
+
+    def __str__(self):  # pylint: disable=too-many-locals
+        if not self._str:
+            # figure out coordinate bounds
+            x_min, x_max = np.inf, -np.inf
+            y_min, y_max = np.inf, -np.inf
+            for x, y in self.airs:
+                x_min = min(x_min, x)
+                x_max = max(x_max, x)
+                y_min = min(y_min, y)
+                y_max = max(y_max, y)
+
+            # compute ranges and offsets
+            x_range = x_max - x_min + 1 + 2 * WALL_THICKNESS
+            x_offset = x_min - WALL_THICKNESS
+            y_range = y_max - y_min + 1 + 2 * WALL_THICKNESS
+            y_offset = y_min - WALL_THICKNESS
+
+            # make an array to store everything
+            # indexed as [j][i] since the rows will be x coords
+            arr = [[None for _ in range(x_range)] for _ in range(y_range)]
+
+            # populate the array appropriately
+            for i in range(x_range):
+                x = i + x_offset
+                for j in range(y_range):
+                    y = j + y_offset
+                    point = (x, y)
+
+                    is_air = point in self.airs
+                    is_box = point in self.boxes
+                    is_goal = point in self.goals
+                    is_player = point == self.player
+
+                    c = PRINTING_SYMBOLS["wall"]
+                    if is_air:
+                        c = PRINTING_SYMBOLS["air"]
+                    if is_box:
+                        c = PRINTING_SYMBOLS["box"]
+                    if is_player:
+                        c = PRINTING_SYMBOLS["player"]
+                    if is_goal:
+                        c = PRINTING_SYMBOLS["goal"]
+                    if is_goal and is_box:
+                        c = PRINTING_SYMBOLS["goal and box"]
+                    if is_goal and is_player:
+                        c = PRINTING_SYMBOLS["goal and player"]
+
+                    arr[j][i] = c
+
+            # form the string, going over rows in reversed order so the y orientation looks right
+            self._str = "\n".join([" ".join(row) for row in reversed(arr)])
+
+        return self._str
+
+    def __hash__(self):
+        """Hash for determining equality"""
+        if not self._hash:
+            self._hash = hash(
+                (
+                    hash(frozenset(self.airs)),
+                    hash(frozenset(self.boxes)),
+                    hash(frozenset(self.goals)),
+                    hash(frozenset(self.player_component)),
+                )
+            )
+        return self._hash
+
+    @property
+    def player_component(self) -> set[Point]:
+        """The locations the player can reach"""
+        if not self._player_component:
+            # explore the locations in the player's connected component
+            self._player_component = set()
+            queue = []
+
+            loc = self.player
+            self._player_component.add(loc)
+            queue.append(loc)
+
+            while queue:
+                x, y = queue.pop()
+                candidate_locs = ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1))
+                for loc in candidate_locs:
+                    # skip if we've already visited it
+                    if loc in self._player_component:
+                        continue
+
+                    # skip if it's not reachable by the player
+                    if loc not in self.airs:
+                        continue
+                    if loc in self.boxes:
+                        continue
+
+                    # otherwise, it's a new location we can visit!
+                    self._player_component.add(loc)
+                    queue.append(loc)
+
+        return self._player_component
+
+    @property
+    def solved(self) -> bool:
+        """Whether the node is in a "solved" state"""
+        return self.boxes == self.goals
+
+    @property
+    def edges(self) -> list[SokobanEdge]:
+        """Edges we can move along from the node"""
+        if not self._edges:
+            self._edges = []
+
+            # iterate over boxes we can push
+            for x_box, y_box in self.boxes:
+                # and possible player pushes to do to this box
+                for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                    # record a valid edge
+                    edge = SokobanEdge(
+                        (dx, dy),
+                        (x_box - dx, y_box - dy),
+                        (x_box, y_box),
+                        (x_box + dx, y_box + dy),
+                    )
+                    if self._check_move(edge):
+                        self._edges.append(edge)
+
+        return self._edges
+
+    def move(self, edge: SokobanEdge) -> Self:
+        """Movement along an edge, returning the new node"""
+        assert self._check_move(edge)
+
+        # start with current node, only making copies of the boxes (which are the only thing that changes)
+        airs = self.airs
+        boxes = self.boxes.copy()
+        goals = self.goals
+
+        # modify to reflect the move
+        boxes.remove(edge.point2)
+        boxes.add(edge.point3)
+        player = edge.point2
+
+        return SokobanNode(airs, boxes, goals, player)
+
+    def unmove(self, edge: SokobanEdge) -> Self:
+        """Undo movement along an edge, returning the new node"""
+        assert self._check_unmove(edge)
+
+        # start with current node, only making copies of the boxes (which are the only thing that changes)
+        airs = self.airs
+        boxes = self.boxes.copy()
+        goals = self.goals
+
+        # modify to reflect the unmove
+        boxes.remove(edge.point3)
+        boxes.add(edge.point2)
+        player = edge.point1
+
+        return SokobanNode(airs, boxes, goals, player)
+
+    def _check_move(self, edge: SokobanEdge) -> bool:
+        """Check validity of moving along an edge"""
+        return (
+            (edge.point1 in self.player_component)
+            and (edge.point2 in self.boxes)
+            and (edge.point3 in self.airs)
+            and (edge.point3 not in self.boxes)
+        )
+
+    def _check_unmove(self, edge: SokobanEdge) -> bool:
+        """Check validity of unmoving along an edge"""
+        return (
+            (edge.point1 in self.airs)
+            and (edge.point1 not in self.boxes)
+            and (edge.point2 in self.player_component)
+            and (edge.point3 in self.boxes)
+        )
+
+
+class SokobanSolver(BFSSolver):
+    """Alias for the preferred solver"""
